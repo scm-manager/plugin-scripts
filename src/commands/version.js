b00f0d31b90321e99ca88c0d22efa9786209a222
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2020-11-17 14:57:24 +0100

    Remove non plugin related scripts

--- src/commands/version.js
+++ src/commands/version.js
@@ -26,7 +26,7 @@ const lerna = require("../lerna");
 const args = process.argv.slice(2);
 
 if (args.length < 1) {
-  console.log("usage ui-scripts version <new-version>");
+  console.log("usage plugin-scripts version <new-version>");
   process.exit(1);
 }
 
